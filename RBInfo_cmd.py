15eb0142359336d0ad834acfc62efef8acbb652d
riccbarelli <99691825+rb7984@users.noreply.github.com>
2022-12-03 14:36:57 +0100

    Info panel Showing

--- RBInfo_cmd.py
+++ RBInfo_cmd.py
@@ -1,13 +1,29 @@
 import os
+import clr
+clr.AddReference("Eto")
+clr.AddReference("Rhino.UI")
+
+from Rhino.UI import *
+from Eto.Forms import *
+from Eto.Drawing import *
 
 __commandname__ = "RBInfo"
 
 def Do():
-    dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(dir, 'html/info.html')
-    open(path, 'r')
+    dialog = Dialog()
+    dialog.Title = "RBPlugin Info"
+    dialog.ClientSize = Size(500, 500)
+    dialog.Padding = Padding(5)
+    dialog.Resizable = False
 
-    return 0
+    image_view = ImageView()
+    dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(dir, 'img/00.png')
+    image_view.Image = Bitmap(path)
+
+    dialog.Content = image_view
+
+    dialog.ShowModal(RhinoEtoApp.MainWindow)
 
 def RunCommand( is_interactive ):
     Do()
