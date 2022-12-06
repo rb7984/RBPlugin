c33ffd2d38e6d955cf7c7b80f7b829254a8c3ad1
riccbarelli <99691825+rb7984@users.noreply.github.com>
2022-12-06 22:02:50 +0100

    Database

--- RBDatabase_cmd.py
+++ RBDatabase_cmd.py
@@ -6,14 +6,12 @@ __commandname__ = "RBDatabase"
 
 def WriteFileXlsx(xlsx, k):
     xlsx += k
-    xlsx += '\n'
+    xlsx += ','
     return xlsx
 
-def WriteFileTxt(txt, k, v):
+def WriteFileTxt(txt, k):
     txt += k
-    txt += ': '
-    txt += v
-    txt += '\n'
+    txt += ','
     return txt
 
 def Database():
@@ -29,27 +27,33 @@ def Database():
     filePath = os.path.join(pathfolderArrive, 'database' + '.txt')
     filePathXlsx = os.path.join(pathfolderArrive, 'database' + '.xlsx')
     
-    #Get Object
+    # Get Object
     rObjs = rs.GetObjects('Oggetti da estrarre')
 
-    txt += 'Name' + '\n'
+    # Set the extraction Keys
+    # AAA Cambiare il criterio di selezione del primo
+    for k in rs.GetUserText(rObjs[0], None):
+        txt += str(k) + ','
+        xlsx += str(k) + ','
+    
+    txt += '\n'
+    xlsx += '\n'
 
-    i = 0
     for rObj in rObjs:        
-        i += 1
-
         keys = rs.GetUserText(rObj, None)
 
         for k in keys:                    
             value = rs.GetUserText(rObj, k)
             
-            #txt = WriteFileTxt(txt, str(k), str(value))  
-            xlsx = WriteFileXlsx(xlsx, str(value))               
+            txt = WriteFileTxt(txt,  str(value))  
+            xlsx = WriteFileXlsx(xlsx, str(value))
+        txt += '\n'
+        xlsx += '\n'
     
     #Write to file
-    # file = open(filePath, "w")
-    # file.write(txt[:len(txt)-1])
-    # file.close()
+    file = open(filePath, "w")
+    file.write(txt[:len(txt)-1])
+    file.close()
     #Write to .xlsx
     file1 = open(filePathXlsx, "w")
     file1.write(xlsx)
